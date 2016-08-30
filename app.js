2e6572f2a486d00ea24237aa3177fe57531b47cc
Valentin 'ThisIsMac' M <thisismac47@gmail.com>
2016-08-30 17:42:54 +0200

    update

--- app.js
+++ app.js
@@ -1,42 +1,20 @@
 var http  = require('http')
 var pmx   = require('pmx').init({
-  http : true
+  http : true,
+  ports : true,
+  network : true
 });
-
 var probe = pmx.probe();
-
-var TIME_INTERVAL = 1000;
-
-var oldTime = process.hrtime();
-
-var histogram = probe.histogram({
-  name        : 'Loop delay',
-  measurement : 'mean',
-  unit        : 'ms'
-});
-
-setInterval(function() {
-  var newTime = process.hrtime();
-  var delay = (newTime[0] - oldTime[0]) * 1e3 + (newTime[1] - oldTime[1]) / 1e6 - TIME_INTERVAL;
-  oldTime = newTime;
-  // Now we update the metric
-  histogram.update(delay);
-}, TIME_INTERVAL);
-
 
 var meter = probe.meter({
   name    : 'req/min',
   seconds : 60
 });
 
-var http  = require('http');
-
 http.createServer(function(req, res) {
   meter.mark();
   res.end('Thanks');
 }).listen(process.env.PORT);
-
-
 
 pmx.action('throw error', {comment : 'Throw a random error'}, function(reply) {
   throw new Error('This error will be caught!');
--- worker/bench.js
+++ worker/bench.js
@@ -5,26 +5,18 @@ var pmx = require('pmx').init({
   http : true
 });
 
-setInterval(function() {
-  console.log('Worker has finished his job.')
-}, 1000);
 
-pmx.action('internals', function(reply) {
-  reply(process.versions)
-});
 
-pmx.action('benchmark', function(reply) { 
+    
   var runOptions = {
     limit: 1000,   
     iterations: 20000 
   };
-  benchrest(process.env.TARGET, runOptions)
+  benchrest(process.env.TARGET || 'http://localhost:3000', runOptions)
     .on('error', function (err, ctxName) { console.error('Failed in %s with err: ', ctxName, err); })
     .on('end', function (stats, errorCount) {
       console.log('error count: ', errorCount);
       console.log('stats', stats);
-
     });
-  reply({ msg : 'Benchmark started'});
-});
+
 
